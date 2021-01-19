a147adfe5a64f5ccf259a91a760a9229f2b9378c
Francesco Pezzati <francesco.pezzati@tech.sdb.it>
2021-01-19 09:21:38 +0100

    Done. Now wildfly came up with postgresql datasource included.

--- wildfly/configure.and.run.wildfly.sh
+++ wildfly/configure.and.run.wildfly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "usage: ./configure.and.run.wildfly.sh PG_HOST PG_PORT PG_DATABASE PG_USER PG_PASSWORD"
 function wait_for_server() {
   until `/opt/jboss/wildfly/bin/jboss-cli.sh -c ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
     sleep 1
@@ -12,7 +13,7 @@ echo "wildfly booted"
 
 echo 'module add --name=org.postgresql --resources=/tmp/postgresql-42.2.0.jar --dependencies=javax.api,javax.transaction.api' | /opt/jboss/wildfly/bin/jboss-cli.sh --connect
 echo '/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgresql",driver-class-name="org.postgresql.Driver")' | /opt/jboss/wildfly/bin/jboss-cli.sh --connect
-echo 'data-source add --jndi-name=java:jboss/datasources/postgres --name=postgres --connection-url=jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE} --driver-name=postgres --user-name=${PG_USER} --password=${PG_PASSWORD}' | /opt/jboss/wildfly/bin/jboss-cli.sh --connect
+echo 'data-source add --jndi-name=java:jboss/datasources/postgres --name=postgres --connection-url=jdbc:postgresql://$1:$2/$3 --driver-name=postgres --user-name=$4 --password=$5' | /opt/jboss/wildfly/bin/jboss-cli.sh --connect
 
 echo "shutting wildfly down"
 /opt/jboss/wildfly/bin/jboss-cli.sh -c ":shutdown"
